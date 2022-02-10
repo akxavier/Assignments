ddb98c26e2a42c552ac99f6b925a2f11aaefe1f9
akxavier <77876814+akxavier@users.noreply.github.com>
2022-02-10 11:42:42 +0530

    Completed Q2.asm

--- S4/HW_Lab/Practice_Lab2/Q2.asm
+++ S4/HW_Lab/Practice_Lab2/Q2.asm
@@ -1,61 +1,94 @@
-; Squaring two digit number
-
 section .data
-msg1 : db 'Enter first digit :'
-l1 : equ $-msg1
-msg2 : db 'Enter second digit :'
-l2 : equ $-msg2
+msg: db 'Enter a two-digit number : '
+l: equ $-msg
+newline: db 10
 
 section .bss
-d1 : resb 1
-d2 : resb 1
-junk : resb 1
-num : resw 1
-square : resd 1
+d1: resb 1
+d2: resb 1
+junk: resb 1
+num: resw 1
+count: resb 1
+temp: resb 1
 
 section .text
+  global _start:
+  _start:
 
-	global _start:
-	_start:
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, msg1
-	mov edx, l1
-	int 80h
+  mov eax,4
+  mov ebx,1
+  mov ecx,msg
+  mov edx,l
+  int 80h
 
-	mov eax, 3
-	mov ebx, 0
-	mov ecx, d1
-	mov edx, 1
-	int 80h
-	
-	mov eax, 3
-	mov ebx, 0
-	mov ecx, junk
-	mov edx, 1
-	int 80h
+  mov eax,3
+  mov ebx,0
+  mov ecx,d1
+  mov edx,1
+  int 80h
 
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, msg2
-	mov edx, l2
-	int 80h
+  mov eax,3
+  mov ebx,0
+  mov ecx,d2
+  mov edx,2
+  int 80h
 
-	mov eax, 3
-	mov ebx, 0
-	mov ecx, d2
-	mov edx, 1
-	int 80h
+  sub byte[d1],30h
+  sub byte[d2],30h
+  mov al,byte[d1]
+  mov bl,10
+  mul bl
+  mov bl,byte[d2]
+  mov bh,0
+  add ax,bx
 
-	mov word[num], 0
-	mov al, byte[d1]
-	sub al, 30h
-	mov bx, 10
-	mul bx
-	
-	add ax, bx
-	mov word[num], ax
-	mov 
-	
-	mul ax
-	mov dx:ax, 	
+  mov bx,ax
+  mul bx
+
+  mov word[num],ax 
+  call print_num
+
+  mov eax,1
+  mov ebx,0
+  int 80h
+
+
+print_num:
+  mov byte[count],0
+  pusha
+  extract_no:
+    cmp word[num],0
+    je print_no
+    inc byte[count]
+    mov dx,0
+    mov ax,word[num]
+    mov bx,10
+    div bx 
+    push dx 
+    mov word[num],ax 
+    jmp extract_no
+
+  print_no:
+    cmp byte[count],0
+    je end_print
+    dec byte[count]
+    pop dx 
+    mov byte[temp],dl 
+    add byte[temp],30h
+
+    mov eax,4
+    mov ebx,1
+    mov ecx,temp
+    mov edx,1
+    int 80h
+    jmp print_no
+
+  end_print:
+    mov eax,4
+    mov ebx,1
+    mov ecx,newline
+    mov edx,1
+    int 80h 
+ 
+popa
+ret
